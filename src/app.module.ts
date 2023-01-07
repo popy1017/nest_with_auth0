b70fdac75a4d1e83040672eb6e7340c6dff318b3
fkn1017 <kosuke.fukano@gmail.com>
2023-01-07 16:22:50 +0900

    AuthzModuleを作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { AuthzModule } from './authz/authz.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, AuthzModule],
   controllers: [AppController],
   providers: [AppService],
 })
